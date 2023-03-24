66c53bc20240c91c1dc2a0744821117551f06bef
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-03-24 10:53:38 +0100

    passing custom component to the as prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import "./App.css";
 import { Text } from "./components/Text";
 
+// because of the typing we used, we can also pass a custom component to 'as', one that we create, like the one underneath
+const Emphasis = ({ children }: { children: React.ReactText }) => {
+  return (
+    <em style={{ background: "yellow", color: "black", fontSize: "40px" }}>
+      {children}
+    </em>
+  );
+};
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +20,8 @@ function App() {
         Google
       </Text>
       <Text>This is a text with no 'as'</Text>
+
+      <Text as={Emphasis}>This is important. You are awesome!!</Text>
     </div>
   );
 }
