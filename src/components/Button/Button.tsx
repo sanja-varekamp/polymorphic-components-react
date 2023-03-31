d6fb32e78e9ce499d35bf157a64ce0488838a6d9
sanja-varekamp <57708942+sanja-varekamp@users.noreply.github.com>
2023-03-31 16:24:31 +0200

    create polymorphic button component with variants

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,43 @@
+import clsx from "clsx";
+import React from "react";
+
+export type ButtonDefaultProps<C extends React.ElementType> = {
+  as?: C;
+  children: React.ReactNode;
+} & React.ComponentPropsWithoutRef<C>;
+
+export type ButtonProps<C extends React.ElementType> = ButtonDefaultProps<C> &
+  Omit<React.ComponentProps<C>, keyof ButtonDefaultProps<C>> &
+  ButtonOptions;
+
+type ButtonVariant = "primary" | "secondary" | "link";
+
+interface ButtonOptions {
+  variant?: ButtonVariant;
+}
+
+const getVariant = (variant: ButtonVariant) => {
+  switch (variant) {
+    case "link":
+      return "button-link";
+    case "secondary":
+      return "button-secondary";
+    default:
+      return "primary";
+  }
+};
+
+export const Button = <C extends React.ElementType>({
+  variant = "primary",
+  children,
+  as,
+  ...restProps
+}: ButtonProps<C>) => {
+  const buttonStyles = clsx("button-primary", getVariant(variant), "className");
+  const Component = as || "button";
+  return (
+    <Component className={buttonStyles} {...restProps}>
+      {children}
+    </Component>
+  );
+};
